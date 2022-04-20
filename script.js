b6829b17652cf812b45339f1436b753134fde920
HominemSanctvm <gabriel.lucca@tutanota.com>
2022-04-20 00:48:13 -0300

    .

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
-const btnNumbers = document.querySelectorAll('.btnNumbers');
-const btnOperators = document.querySelectorAll('.btnOperators');
-const btnAC = document.getElementById('btnAC'); 
-const inOperation = document.getElementById('inOperation');
-const outResults = document.getElementById('outResults');
+const btnCalc = document.querySelectorAll('.btnCalc');
+const btnAllClear = document.getElementById('btnAllClear'); 
 const equalOperator = document.getElementById('equalOperator');
 let firstNumber = 0;
-let SecondNumber = 10;
-let choosedOperator = '';
+let secondNumber = 0;
 
 function calcSum(a,b) {
 	return a + b;
-};
+}
 
 function calcMinus(a,b) {
 	return a - b;
@@ -25,44 +21,49 @@ function calcDivide(a, b) {
 }
 
 function calcAllClear() {
-	inOperation.value = '';
-	outResults.value = 0;
+	inDisplay.value = '';
+	outDisplay.value = '';
+	firstNumber = 0;
+	secondNumber = 0;
+}
+
+function disassembly(displayRaw) {
+	const indexOfOperator = displayRaw.search(/\D/);
+	return indexOfOperator;
 }
 
 function operate() {
-//	switch (choosedOperator) {
-//		case '+':
-//			outResults.value = calcSum(firstNumber, SecondNumber);
-//			inOperation.value = '';
-//			break;
-//		case '-':
-//			outResults.value = calcMinus(firstNumber, SecondNumber);
-//			inOperation.value = '';
-//			break;
-//		case 'X':
-//			outResults.value = calcMultiply(firstNumber, SecondNumber);
-//			inOperation.value = '';
-//			break;
-//		case '/':
-//			outResults.value = calcDivide(firstNumber, SecondNumber);
-//			inOperation.value = '';
-//			break;
-//	}
+	const inDisplay = document.getElementById('inDisplay');
+	const outDisplay = document.getElementById('outDisplay');
+	const userInput = inDisplay.value;
+	
+	const indexOfOperator = disassembly(userInput);
+	const operator = userInput.charAt(indexOfOperator);
+
+	firstNumber = parseInt(userInput.slice(0, indexOfOperator));
+	secondNumber = parseInt(userInput.slice(indexOfOperator+1));
+	
+	switch (operator) {
+		case '+':
+			outDisplay.value = calcSum(firstNumber, secondNumber);
+			break;
+		case '-':
+			outDisplay.value = calcMinus(firstNumber, secondNumber);
+			break;
+		case '*':
+			outDisplay.value = calcMultiply(firstNumber, secondNumber);
+			break;
+		case '/':
+			outDisplay.value = calcDivide(firstNumber, secondNumber);
+			break;
+	}
 }
 
-for (const button of btnNumbers) {
+for (const button of btnCalc) {
 	button.addEventListener('click', function() {
-		inOperation.value += button.value;
+		inDisplay.value += button.value;
 	});
-};
+}
 
-for (const operators of btnOperators) {
-	operators.addEventListener('click', function() {
-//		firstNumber = parseInt(inOperation.value);
-//		choosedOperator = operators.value;
-//		inOperation.value += operators.value;
-	});
-};
-
-btnAC.addEventListener('click', calcAllClear);
+btnAllClear.addEventListener('click', calcAllClear);
 equalOperator.addEventListener('click', operate);
