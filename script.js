01dc3b16612a2ac60a2469676afc9d0394442bc9
HominemSanctvm <gabriel.lucca@tutanota.com>
2022-04-27 12:50:43 -0300

    .

--- script.js
+++ script.js
@@ -1,58 +1,95 @@
-const outDisplay = document.getElementById('outDisplay');
 const btnNumbers = document.querySelectorAll('.btnNumbers');
 const btnOperators = document.querySelectorAll('.btnOperators');
 const btnAllClear = document.getElementById('btnAllClear'); 
 const btnEqual = document.getElementById('btnEqual');
-let displayNumber = '';
-let userInputNumbers = [];
-let operatorChoice = '';
+const btnStyle = document.querySelectorAll('.btnStyle');
+const outDisplayCalculator = document.getElementById('outDisplayCalculator');
+let displayCalculator = '';
+let userInputNumbers = ['0','0'];
+let index = 0;
+let userOperatorChoice = '';
+let finishedCalc = false;
 
 function calcSum(a,b) {
-	return a + b;
+	return Number(a) + Number(b);
 }
 
 function calcMinus(a,b) {
-	return a - b;
+	return Number(a) - Number(b);
 }
 
 function calcMultiply(a, b) {
-	return a * b;
+	return Number(a) * Number(b);
 }
 
 function calcDivide(a, b) {
-	return a / b;
+	return Number(a) / Number(b);
 }
 
 function operate() {
-	userInputNumbers.push(Number(outDisplay.value));
-	for (let i = 0; i < userInputNumbers.length; i++) {
-		alert(userInputNumbers[i]);}
-//	switch (operatorChoice) {
-	//	case '+':
-	//	case '-':
-	//	case '*':
-	//	case '/':
-	//}			
+	switch (userOperatorChoice) {
+		case '+':
+			displayCalculator = userInputNumbers.reduce(calcSum);
+			break;
+		case '-':
+			displayCalculator = userInputNumbers.reduce(calcMinus);
+			break;
+		case '*':
+			displayCalculator = userInputNumbers.reduce(calcMultiply);
+			break;
+		case '/':
+			displayCalculator = userInputNumbers.reduce(calcDivide);
+			break;
+	}			
+	outDisplayCalculator.textContent = displayCalculator;
+	userInputNumbers[0] = '0';
+	userInputNumbers[1] = '0';
+	index = 0;
+	finishedCalc = true;
 }
 
-function display(something) {
-	const outDisplay = document.getElementById('outDisplay');
-	outDisplay.value = displayNumber;
+function initialize() {
+	outDisplayCalculator.textContent = '0';
+}
+
+function sound(src) {
+  this.sound = document.createElement("audio");
+  this.sound.src = src;
+  document.body.appendChild(this.sound);
+  this.play = function(){
+    this.sound.play();
+  }
 }
 
 btnNumbers.forEach((button) => {
 	button.addEventListener('click', function() {
-		displayNumber += button.value;
-		display(displayNumber);
+		if (finishedCalc)
+			displayCalculator = '';
+
+		displayCalculator += button.value;
+		userInputNumbers[index] += Number(displayCalculator);
+		outDisplayCalculator.textContent = displayCalculator;
 	});
 })
 
 btnOperators.forEach((operator) => {
 	operator.addEventListener('click', function() {
-		operatorChoice = operator.value;
-		userInputNumbers.push(Number(outDisplay.value));
-		displayNumber = '';
+
+		if (finishedCalc)
+			userInputNumbers[0] = Number(displayCalculator);
+
+		userOperatorChoice = operator.value;
+		index = 1;
+		displayCalculator = '';
 	});
 })
 
+btnStyle.forEach((button) => {
+	button.addEventListener('click', function() {
+		const beep = new sound('short-beep.wav');
+		beep.play();
+	});
+})
+
+initialize();
 btnEqual.addEventListener('click', operate);
