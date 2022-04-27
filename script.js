41c8714adbf71daea371b1c68b53ca74085da271
HominemSanctvm <gabriel.lucca@tutanota.com>
2022-04-27 13:46:40 -0300

    .

--- script.js
+++ script.js
@@ -11,19 +11,43 @@ let userOperatorChoice = '';
 let finishedCalc = false;
 
 function calcSum(a,b) {
-	return Number(a) + Number(b);
+	return parseFloat(a) + parseFloat(b);
 }
 
 function calcMinus(a,b) {
-	return Number(a) - Number(b);
+	return parseFloat(a) - parseFloat(b);
 }
 
 function calcMultiply(a, b) {
-	return Number(a) * Number(b);
+	return parseFloat(a) * parseFloat(b);
 }
 
 function calcDivide(a, b) {
-	return Number(a) / Number(b);
+	return parseFloat(a) / parseFloat(b);
+}
+
+function passNumber(button) {
+	if (finishedCalc) {
+		displayCalculator = '';
+		finishedCalc = false;
+	}
+
+	displayCalculator += button.value;
+	userInputNumbers[index] += button.value;
+	outDisplayCalculator.textContent = displayCalculator;
+
+}
+
+function passOperator(operator) {
+	if (finishedCalc) {
+		userInputNumbers[0] = displayCalculator;
+		finishedCalc = false;
+	}
+
+	userOperatorChoice = operator.value;
+	index = 1;
+	displayCalculator = '';
+
 }
 
 function operate() {
@@ -48,6 +72,19 @@ function operate() {
 	finishedCalc = true;
 }
 
+function clear() {
+	
+}
+
+function allClear() {
+	outDisplayCalculator.textContent = '0';
+	displayCalculator = '';
+	userInputNumbers[0] = '0';
+	userInputNumbers[1] = '0';
+	index = 0;
+	finishedCalc = false;
+}
+
 function initialize() {
 	outDisplayCalculator.textContent = '0';
 }
@@ -63,30 +100,17 @@ function sound(src) {
 
 btnNumbers.forEach((button) => {
 	button.addEventListener('click', function() {
-		if (finishedCalc) {
-			displayCalculator = '';
-			finishedCalc = false;
-		}
-
-		displayCalculator += button.value;
-		userInputNumbers[index] += Number(button.value);
-		outDisplayCalculator.textContent = displayCalculator;
+		passNumber(button);
 	});
 })
 
 btnOperators.forEach((operator) => {
 	operator.addEventListener('click', function() {
-
-		if (finishedCalc) {
-			userInputNumbers[0] = Number(displayCalculator);
-			finishedCalc = false;
-		}
-
-		userOperatorChoice = operator.value;
-		index = 1;
-		displayCalculator = '';
+		passOperator(operator);
 	});
 })
+
+btnAllClear.addEventListener('click', allClear);
 
 btnStyle.forEach((button) => {
 	button.addEventListener('click', function() {
